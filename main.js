1969f0d9ace857aa802b70b3f6c95a81b712045b
Sunnatjon <sunnattoxtaqulov4444@gmail.com>
2024-04-03 19:30:10 +0300

    algoritim

---
+++ main.js
@@ -0,0 +1,59 @@
+// ++++++++++1+++++++++++
+let numbers = +prompt("  Son kiriting " , "")
+  if(numbers % 3 == 0 ) {
+    console.log(` Fizz`);
+  } else if ( numbers % 5 == 0){
+    console.log("Bazz");
+  } else if(numbers % 3 == 0 && numbers % 3 == 0){
+    console.log("FizzBazz");
+  }else{
+    console.log("Siz adashdimgiz");
+  }
+
+
+// +++++++++++2+++++++++++++
+// let num =  alert("Uchta sonni ketma ketligda kiritiing  ")
+// let first = +prompt("Ixtiyoriy birinchi sonni kiriting !" , "")
+// let second = +prompt("Ixtiyoriy ikkinchi sonni kiriting !" , "")
+// let three = +prompt("Ixtiyoriy uchinchi sonni kiriting !" , "")
+
+// if((((first + second + three) / 3 ) % 2 == 0)){
+//     console.log((first +  second + three) / 3);
+// }else (console.log(Math.floor((first + second + three) / 3)))
+
+//   3
+
+// 4
+// find
+// let arr = [ 1 , 3, 5, 2 , 4]
+
+
+
+// let number = +prompt("Son kiriting" , "")
+// if( number % 2 == 0 &&  Math.sqrt(number) ){
+//     console.log(number);
+// }else{
+//     console.log("Bu sondan ildiz chiqmaydi ");
+// }
+
+// 6
+// let massive = [ 8,5,3,2,7,18].reverse()
+// console.log(massive);
+
+// 7
+
+// let ogoh = alert("Ikkita son va bitta amal kiriting")
+// let s =  +prompt("Istalgan bir son kiriting" , "")
+// let a = prompt("Istalgan bir amal kiriting" , "")
+// let q = +prompt("Istalgan ikkinchi sonni  kiriting" , "")
+//   switch (q) {
+//     case  "+":
+//        console.log(a + b); 
+//         break;
+//         case "-":
+//             console.log(a - b); 
+//              break;
+//     default:
+//         console.log("Amal xatoo");
+//         break;
+//   }
